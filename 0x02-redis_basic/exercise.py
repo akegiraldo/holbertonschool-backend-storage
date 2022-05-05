795255fb58bceed2ded32a28bb71739954ef0eab
akegiraldo <ake.giraldo@gmail.com>
2022-05-04 22:53:33 -0500

    [task 2] - Incrementing values

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,8 +2,22 @@
 """Script with a class for manipulate redis"""
 
 from typing import Callable, Optional, Union
+from functools import wraps
 import redis
 import uuid
+
+
+def count_calls(method: Callable) -> Callable:
+    """Decorator for count functions calls"""
+    @wraps(method)
+    def decorator(*args, **kwargs):
+
+        key = method.__qualname__
+        cache = args[0]
+        cache._redis.incr(key, 1)
+
+        return method(*args, **kwargs)
+    return decorator
 
 
 class Cache:
@@ -14,13 +28,14 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @count_calls
     def store(self, data: Union[str, Union[bytes, Union[int, float]]]) -> str:
         """Method for generate a key and save in cache"""
         key = str(uuid.uuid1())
         self._redis.set(key, data)
         return key
 
-    def get(self, key: str, fn: Optional[Callable]) -> \
+    def get(self, key: str, fn: Optional[Callable] = None) -> \
             Optional[Union[str, Union[bytes, Union[int, float]]]]:
         """Method to get value from cache"""
         value = self._redis.get(key)
@@ -28,10 +43,12 @@ class Cache:
             return fn(value)
         return value
 
-    def get_str(self, key: str) -> Optional[str]:
+    def get_str(self, key: str) -> \
+            Optional[Union[str, Union[bytes, Union[int, float]]]]:
         """Method to get str value from cache"""
         return self.get(key, str)
 
-    def get_int(self, key: str) -> Optional[int]:
+    def get_int(self, key: str) -> \
+            Optional[Union[str, Union[bytes, Union[int, float]]]]:
         """Method to get int value from cache"""
         return self.get(key, int)
